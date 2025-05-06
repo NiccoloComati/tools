a92808da8b585fb66a64f88d0ca46d0c00b02ae4
NiccoloComati <76107977+NiccoloComati@users.noreply.github.com>
2025-05-06 14:23:24 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# tools/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="tools",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "boto3",
+        "tqdm"
+    ],
+)
